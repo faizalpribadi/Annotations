ec8f38024b9a4708fd764db9a1fb31deac62d731
FaizalPribadi <theblackshinobi@programmer.net>
2012-12-27 14:44:45 +0700

    @Lazy Annotations For Lazy Options Class Parameters Doc Comment

---
+++ Lazy.php
@@ -0,0 +1,25 @@
+<?php
+namespace Mozart\Library\Annotations;
+
+/*
+ * This file is a part of Mozart PHP Small MVC Framework
+ *
+ * (C) Faizal Pribadi - Gesture Media Ltd - 2012
+ *
+ * For the full copyright and license information, please view the LICENSE
+ *
+ * file that was distributed with this source code.
+ */
+
+/**
+ * Used the injector object|class or property as lazy-loaded
+ *
+ * @author  Faizal Pribadi  <faizal_pribadi@aol.com>
+ *
+ * @Annotation
+ * @Target({"CLASS", "PROPERTY"})
+ */
+final class Lazy
+{
+
+}
