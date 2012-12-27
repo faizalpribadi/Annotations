f7e0f3a8e24acac6a0704b7455f4d4599ac4ad04
FaizalPribadi <theblackshinobi@programmer.net>
2012-12-27 14:46:56 +0700

    @ValueObject Annotations {@inheritdoc}

---
+++ ValueObject.php
@@ -0,0 +1,25 @@
+<?php
+namespace Mozart\Library\Annotations;
+
+/*
+ * This file is a part of Mozart PHP Small MVC Framework
+ *
+ * (C) Faizal Pribadi - Gesture Media Ltd - 2012
+ *
+ * For the full copyright and license information, please view the LICENSE
+ *
+ * file that was distributed with this source code.
+ */
+
+/**
+ * Marks the annotation class as value object
+ *
+ * @author  Faizal Pribadi  <faizal_pribadi@aol.com>
+ *
+ * @Annotation
+ * @Target("CLASS")
+ */
+final class ValueObject
+{
+
+}
