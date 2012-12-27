802a0e6dbf3db57a9d65dbaf25dc1766123a4fe9
FaizalPribadi <theblackshinobi@programmer.net>
2012-12-27 14:44:20 +0700

    @Inject Annotations For Inject The Some Other Class

---
+++ Inject.php
@@ -0,0 +1,25 @@
+<?php
+namespace Mozart\Library\Annotations;
+
+/*
+ * This file is a part of Mozart PHP Small MVC Framework
+ *
+ * (C) Faizal Pribadi - Gesture Media Ltd - 2012
+ *
+ * For the full copyright and license information, please view the LICENSE
+ *
+ * file that was distributed with this source code.
+ */
+
+/**
+ * Inject annotation class will be used dependency injection property
+ *
+ * @author  Faizal Pribadi  <faizal_pribadi@aol.com>
+ *
+ * @Annotation
+ * @Target("PROPERTY")
+ */
+final class Inject
+{
+
+}
