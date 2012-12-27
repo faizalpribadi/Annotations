51df1b58463f6d5e192881779b2896f969ea541d
FaizalPribadi <theblackshinobi@programmer.net>
2012-12-27 14:43:54 +0700

    @Access Annotations For Accessing The Class

---
+++ Access.php
@@ -0,0 +1,25 @@
+<?php
+namespace Mozart\Library\Annotations;
+
+/*
+ * This file is a part of Mozart PHP Small MVC Framework
+ *
+ * (C) Faizal Pribadi - Gesture Media Ltd - 2012
+ *
+ * For the full copyright and license information, please view the LICENSE
+ *
+ * file that was distributed with this source code.
+ */
+
+/**
+ * Access the method of the object with annotation
+ *
+ * @author  Faizal Pribadi  <faizal_pribadi@aol.com>
+ *
+ * @Annotation
+ * @Target("METHOD")
+ */
+final class Access
+{
+
+}
